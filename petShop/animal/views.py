97f7d88465b6f3d3f2f703b182790cf8b4be17cc
AmandaMantovani <amandamantovani.09@gmail.com>
2014-11-18 22:46:02 -0200

    Validar

--- petShop/animal/views.py
+++ petShop/animal/views.py
@@ -5,3 +5,17 @@ from animal.forms import AnimalForm
 def index(request):
 	form = AnimalForm();
 	return render(request, 'index.html', {'form':form})
+
+def validar(request):
+	if request.method == 'POST':
+		form = AnimalForm(request.POST)
+
+		if form.is_valid(): #valida o tamanho e o tipo do campo de acordo com o que foi determinado
+			animal = Animal(**form.cleaned_data) #esse cleaned_data transforma campos em branco para campos NULL, o ** significa lista
+			animal.save()
+
+			animal = Animal.objects.all().order_by('nome') #select * from
+			
+			return render(request,'validar.html',{'form':form, 'animal':animal})
+		else:
+			return render(request,'index.html',{'form':form})
